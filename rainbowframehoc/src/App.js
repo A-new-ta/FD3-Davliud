8b86d748f053bd2dde4035f170b84cd844aeddfb
A-new-ta <urievna86@gmail.com>
2021-12-28 09:06:59 +0300

    feat:complete task

--- rainbowframehoc/src/App.js
+++ rainbowframehoc/src/App.js
@@ -6,10 +6,16 @@ function App() {
 
   const colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
   const FramedDoubleButton = withRainbowFrame(colors)(DoubleButton);
+  const cbPress = (num) => alert(num);
 
   return (
-    <div className="App">
-      
+    <div>
+      <DoubleButton caption1='Однажды' caption2='пору' onPress={cbPress}>
+        в студеную зимнюю
+      </DoubleButton>
+      <FramedDoubleButton caption1='Я из лесу' caption2='мороз' onPress={cbPress}>
+        , был сильный мороз
+      </FramedDoubleButton>
     </div>
   );
 }
--- rainbowframehoc/src/components/DoubleButton/DoubleButton.js
+++ rainbowframehoc/src/components/DoubleButton/DoubleButton.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const DoubleButton = (props) => (
     <div>
-        <input />
+        <input value={props.caption1} onClick={() => props.onPress('1')} />
         {props.children}
-        <input />
+        <input value={props.caption2} onClick={() => props.onPress('2')} />
     </div>
-)
+);
+
+export default DoubleButton;
 
